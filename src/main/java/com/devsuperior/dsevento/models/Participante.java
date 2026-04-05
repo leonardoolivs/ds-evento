ce2f4e3e14151906f28697348210f28e0d4240a5
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-04-04 21:30:14 -0300

    feat: Adiciona entity Participante-ORM

---
+++ src/main/java/com/devsuperior/dsevento/models/Participante.java
@@ -0,0 +1,52 @@
+package com.devsuperior.dsevento.models;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "TB_PARTICIPANTE")
+public class Participante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @Column(unique = true)
+    private String email;
+
+    private List<Participante> participantes = new ArrayList<>();
+
+    public Participante(Long id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Participante(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
